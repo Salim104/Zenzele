d7637ee8780ead577b72c8e5ee179998ead118d9
salim104 <salimshaban885@gmail.com>
2022-11-07 18:01:46 +0200

    pictures

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import './hero.css';
+import styled from 'styled-components'
+
+
+const MainHeading = styled.h1`
+      font-family: 'Inter';
+font-style: normal;
+font-weight: 800;
+font-size: 72px;
+line-height: 77px;
+color: #1D7E38;
+
+`
 
 function Hero() {
   return (
     <div className='hero_container'>
 
-      <h1>Zenzele</h1>
+      <div className='hero_disc'>
+           <MainHeading>ZENZELE</MainHeading>
+      <p className='maindisc'> Zenzele is a community based insurance which seeks </p>
+      <p>
+        to enable low income communities to have access to <br/>  
+        quality health care services
+      </p>
+      </div>
+      <div className='hero_img'>
+
+      </div>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Navbar from '../Components/Navbar.js'
-import styeled  from 'styled-components'
+import styled  from 'styled-components'
 import Hero from '../Components/Hero.js'
 
 
-const Wrapper_gl = styeled.section`
+const Wrapper_gl = styled.section`
        padding-inline: 2rem;
        background-color:  #F5F5F5;
        padding-block:0.2rem;
 `
-const Wrapper_g = styeled.section`
+const Wrapper_g = styled.section`
        padding-inline: 2rem;
        background-color:  #CDDEA8;
        padding-block:0.2rem;
